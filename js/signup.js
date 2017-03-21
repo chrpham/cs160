2d36557279b33f7b955180201ea71d2b81d49334
ruifengT <ruifeng.tian@sjsu.edu>
2017-03-20 23:14:52 -0700

    Add server connect with signup.html
    For login and registeration.html will be later

--- js/signup.js
+++ js/signup.js
@@ -41,4 +41,47 @@ document.querySelector('.cont_form_sign_up').style.display = "none";
 document.querySelector('.cont_form_login').style.display = "none";
 },500);
 
-  }
+}
+
+function server_login(){
+    // TBA
+    window.alert('server_login');
+}
+
+function server_signup(){
+    var id = document.getElementById('user_id').value;
+    var name = document.getElementById('user_name').value;
+    var pass = document.getElementById('user_password').value;
+    var conf_pass = document.getElementById('confirm_password').value;
+
+    if(password != conf_pass){
+        // check if password match
+        alert('Password not match!');
+    }
+    else{
+        // it's better to do with ajax, but this also works
+        post('php/Signup.php', {user_id: id, user_name: name, user_password: password});
+    }
+}
+
+function post(path, params, method) {
+    method = method || "post"; // Set method to post by default
+
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
--- php/Signup.php
+++ php/Signup.php
@@ -3,15 +3,11 @@ require "Config.php";
 
 $user_id = $_POST['user_id'];
 $user_password = $_POST['user_password'];
-$user_name = $_POST['user_name'];
 $user_email = $_POST['user_email'];
-$user_phone = $_POST['user_phone'];
-$user_address = $_POST['user_address'];
-$user_creditcard = $_POST['user_creditcard'];
 $datetime = date('YmdHis');
 
 $sql_insert_logins = "INSERT INTO `logins`(`login_user_id`, `login_user_password`) VALUES ('" . $user_id . "','" . $user_password . "')";
-$sql_insert_users = "INSERT INTO `users`(`user_id`, `user_name`, `user_email`, `user_phone`, `user_address`, `user_creditcard`, `user_create_stmp`) VALUES ('" . $user_id . "','" . $user_name . "','" . $user_email . "','" . $user_phone . "','" . $user_address. "','" . $user_creditcard . "','" . $datetime . "')";
+$sql_insert_users = "INSERT INTO `users`(`user_id`, `user_email`, `user_create_stmp`) VALUES ('" . $user_id . "','" . $user_email . "','" . $datetime . "')";
 
 if(!mysql_query($sql_insert_logins, $conn)){
     echo "Error: " . mysql_error($conn);
@@ -22,7 +18,7 @@ else{
     }
     else{
         // change to user dashboard when it's done
-        header("Location: ../cs160/php/Testserver.php");
+        header("Location: ../php/Testserver.php");
     }
 }
 
